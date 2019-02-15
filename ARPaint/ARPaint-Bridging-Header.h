c7519e2ece647a77aaaf0d4b0b2a2dee00501f91
David Crooks <crodave@gmail.com>
2019-02-15 13:07:37 +0100

    Initial Commit

---
+++ ARPaint/ARPaint-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ARPaint-Bridging-Header.h
+//  ARPaint
+//
+//  Created by David Crooks on 15/02/2019.
+//  Copyright © 2019 David Crooks. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
